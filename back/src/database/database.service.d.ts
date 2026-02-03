364e87a03895479b2e161543e064ee8493330f42
Matheus Togni <informatica4@zagonel.com.br>
2026-02-03 08:35:51 -0300

    removido os arquivos do build

--- back/src/database/database.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { QueryResult } from 'pg';
-export declare class DatabaseService implements OnModuleInit, OnModuleDestroy {
-    private pool;
-    onModuleInit(): Promise<void>;
-    onModuleDestroy(): Promise<void>;
-    query(text: string, params?: any[]): Promise<QueryResult>;
-}
